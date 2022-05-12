63e4ecba20201426d37f5651294ecded88511af8
Naxred <rexkid2001@gmail.com>
2022-05-11 23:18:57 -0500

    Se agregaron las clases en PHP

---
+++ CursoPOOUber/PHP/route.php
@@ -0,0 +1,8 @@
+<?php	
+class Route 
+{
+    public $id = integer;
+    public $init = array();
+    public $end = array();
+}
+?>
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,3 +1,2 @@
 if __name__ == "__main__":
     print("Hola mundo")
-    print("Este es un mensaje nuevo")
