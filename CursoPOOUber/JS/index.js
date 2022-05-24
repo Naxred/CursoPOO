c3ec8f9dfdecdbe11f3fb51bdaa2abb633f55f7e
Naxred <rexkid2001@gmail.com>
2022-05-24 17:26:30 -0500

    Se agregaron las clases hijas en JS

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,8 @@
 var car = new Car("QWERTY123", new Account("Andres Herrera", "PERAS333"))
 car.passenger = 4;
 car.printDataCar();
+
+
+var uberX = new UberX("QWERTY123", new Account("Juan Gabriel", "1958795"), "Chevrolet", "Cruze");
+uberX.passenger = 4;
+uberX.printDataCar();
